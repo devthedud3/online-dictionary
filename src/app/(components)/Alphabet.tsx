430f0e7adf87ff56e75c5e009eca413d7e083cd7
corey <corey.cushnie@gmail.com>
2024-07-15 18:04:13 -0400

    alphabet fix

--- src/app/(components)/Alphabet.tsx
+++ src/app/(components)/Alphabet.tsx
@@ -20,15 +20,15 @@ const Alphabet: React.FC<AlphabetProps> = ({ words }) => {
             id={`letter-${letter}`}
             className={`w-full justify-between py-24 ${
               index !== aZ.length - 1 && "border-b"
-            } border-black grid grid-cols-4`}
+            } border-zinc-700 grid lg:grid-cols-4`}
           >
-            <div className="relative">
+            <div className="relative pb-10 lg:pb-0">
               <p className="text-5xl ">{letter}</p>
-              <p className="absolute text-rose-600 top-0 left-8 text-md font-black">
+              <p className="absolute text-[--primary] top-0 left-8 text-md font-black">
                 {groupedWords.length}
               </p>
             </div>
-            <div className="w-full col-span-3 gap-6 grid grid-cols-2">
+            <div className="w-full col-span-3 gap-1 grid lg:grid-cols-2">
               {groupedWords.length > 0 ? (
                 groupedWords.map((word) => (
                   <Link
@@ -37,7 +37,7 @@ const Alphabet: React.FC<AlphabetProps> = ({ words }) => {
                     className="group w-fit"
                   >
                     <p className="cursor-pointer">{word}</p>
-                    <div className="transition-all duration-300 h-2 border-b-2 border-b-red-400 group-hover:w-full w-0" />
+                    <div className="transition-all duration-300 h-2 border-b-2 border-b-[--primary] group-hover:w-full w-0" />
                   </Link>
                 ))
               ) : (
