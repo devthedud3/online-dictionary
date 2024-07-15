887a1f0b5e74ec97ef844d1f952ac3de7bd6de9b
corey <corey.cushnie@gmail.com>
2024-07-15 18:04:01 -0400

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
   const filePath = path.join(process.cwd(), "lib", "/words.txt");
   const words = fs.readFileSync(filePath, "utf8").split("\n");
 
-  console.log(words);
-
   return (
-    <main className="flex flex-col items-center justify-between p-40 pt-0 h-screen">
+    <main className="flex flex-col items-center justify-between p-10 lg:p-40 pt-0  h-screen">
       <section>
-        <div className="grid grid-cols-4 border-b border-black py-20">
-          <p className="col-span-1">The Gloss</p>
-          <div className="col-span-3 h-72 flex flex-col justify-between">
-            <p className="text-2xl w-2/3">
-              <span className="text-rose-600 font-semibold">
+        <div className="grid lg:grid-cols-4 border-b border-zinc-700 py-10">
+          <p className="col-span-1 text-xs font-sans pb-10 lg:py-0 text-nowrap">
+            The Gloss
+          </p>
+          <div className="col-span-3 flex flex-col justify-between space-y-20">
+            <p className="text-xl w-2/3 text-gray-500">
+              <span className="text-white font-semibold">
                 All things dev in words.{" "}
               </span>
               Browse definitions and learn improve your knowledge and
