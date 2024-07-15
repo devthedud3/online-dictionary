d4aaee7fe96ccda7d08977df544fa9d65a5ea18e
corey <corey.cushnie@gmail.com>
2024-07-15 16:52:05 -0400

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import Alphabet from "./(components)/Alphabet";
+import fs from "fs";
+import path from "path";
 import { Search } from "./(components)/Search";
 
 export default function Home() {
+  const filePath = path.join(process.cwd(), "lib", "/words.txt");
+  const words = fs.readFileSync(filePath, "utf8").split("\n");
+
+  console.log(words);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <div>
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-6">
-          <p className="fixed left-0 top-0 flex w-full justify-center bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl">
-            Online Dictionary&nbsp;
-          </p>
+    <main className="flex flex-col items-center justify-between p-40 pt-0 h-screen">
+      <section>
+        <div className="grid grid-cols-4 border-b border-black py-20">
+          <p className="col-span-1">The Gloss</p>
+          <div className="col-span-3 h-72 flex flex-col justify-between">
+            <p className="text-2xl w-2/3">
+              <span className="text-rose-600 font-semibold">
+                All things dev in words.{" "}
+              </span>
+              Browse definitions and learn improve your knowledge and
+              understanding of whatever you need to further your career.
+            </p>
+            <Search />
+          </div>
         </div>
-        <Alphabet />
-      </div>
+        <Alphabet words={words} />
+      </section>
     </main>
   );
 }
