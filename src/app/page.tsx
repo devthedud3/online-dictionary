f81c3795a21215438186b9dbf2aad896b6dec368
corey <corey.cushnie@gmail.com>
2024-07-15 19:53:17 -0400

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,23 @@ export default function Home() {
   const words = fs.readFileSync(filePath, "utf8").split("\n");
 
   return (
-    <main className="flex flex-col items-center justify-between p-10 lg:p-40 pt-0  h-screen">
-      <section>
-        <div className="grid lg:grid-cols-4 border-b border-zinc-700 py-10">
-          <p className="col-span-1 text-xs font-sans pb-10 lg:py-0 text-nowrap">
-            The Gloss
+    <main className="flex flex-col items-center justify-between lg:p-40 pt-0 h-screen">
+      <div className="grid lg:grid-cols-4 border-b px-10 py-20 border-zinc-700">
+        <p className="col-span-1 text-xs font-sans pb-10 lg:py-0 text-nowrap">
+          The Gloss
+        </p>
+        <div className="lg:col-span-3 w-fit flex flex-col justify-between space-y-20">
+          <p className="text-xl w-2/3 text-gray-500">
+            <span className="text-white font-semibold">
+              All things dev in words.{" "}
+            </span>
+            Browse definitions and learn improve your knowledge and
+            understanding of whatever you need to further your career.
           </p>
-          <div className="col-span-3 flex flex-col justify-between space-y-20">
-            <p className="text-xl w-2/3 text-gray-500">
-              <span className="text-white font-semibold">
-                All things dev in words.{" "}
-              </span>
-              Browse definitions and learn improve your knowledge and
-              understanding of whatever you need to further your career.
-            </p>
-            <Search />
-          </div>
+          <Search />
         </div>
-        <Alphabet words={words} />
-      </section>
+      </div>
+      <Alphabet words={words} />
     </main>
   );
 }
