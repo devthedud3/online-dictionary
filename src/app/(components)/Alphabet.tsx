605edf4b2db1239d041073d6e5a8bfc8ba8c6a0e
corey <corey.cushnie@gmail.com>
2024-07-15 16:52:14 -0400

    alphabet build

--- src/app/(components)/Alphabet.tsx
+++ src/app/(components)/Alphabet.tsx
@@ -1,136 +1,71 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
-import { Search } from "./Search";
 import axios from "axios";
+import Link from "next/link";
 
-type AlphabetProps = {};
+type AlphabetProps = {
+  words: string[];
+};
 
-const abc = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-
-const Alphabet = ({}: AlphabetProps) => {
-  const [query, setQuery] = useState<string>(abc[0]);
+const Alphabet: React.FC<AlphabetProps> = ({ words }) => {
+  const aZ = "#abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+  const [query, setQuery] = useState<string>(aZ[0]);
   const [data, setData] = useState<any>([]);
   const [current, setCurrent] = useState<number>(0);
 
-  useEffect(() => {
-    try {
-      const options = {
-        method: "GET",
-        url: `https://wordsapiv1.p.rapidapi.com/words/${
-          query ? query : abc[current]
-        }`,
-        params: {
-          letterPattern: "^a.{4}$",
-          pronunciationpattern: ".*æm$",
-          limit: "100",
-          page: "1",
-          hasDetails: "hasDetails",
-        },
-        headers: {
-          "X-RapidAPI-Key": process.env.RAPID_KEY,
-          "X-RapidAPI-Host": process.env.RAPID_HOST,
-        },
-      };
-      const getData = async () => {
-        const response = await axios(options);
-        setData(response.data.results);
-      };
-      console.log(data);
-      getData();
-    } catch (error) {
-      setData([]);
-      console.log(error);
-    }
-  }, [query]);
-
-  const handleClick = (e: number) => {
-    setCurrent(e);
-    setQuery("");
-  };
-
-  const alphabet = (val: string, idx: number) => {
-    return (
-      <div
-        key={idx}
-        onClick={() => handleClick(idx)}
-        className={`p-4 ${
-          idx == current && "bg-gray-100"
-        } rounded hover:border-gray-100 hover:bg-gray-100 cursor-pointer mb-3 text-2xl group-hover:translate-x-1 transition-transform  motion-reduce:transform-none font-semibold`}
-      >
-        <h2>{val}</h2>
-      </div>
-    );
-  };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (query) {
+  //       try {
+  //         const { data } = await axios.get(
+  //           `https://api.dictionaryapi.dev/api/v2/entries/en/${query.toLowerCase()}`
+  //         );
+  //         setData(data);
+  //       } catch (error) {
+  //         setData([]);
+  //         console.error("Error fetching data:", error);
+  //       }
+  //     }
+  //   };
+  //   fetchData();
+  // }, [query]);
 
   return (
-    <div className="flex flex-col items-center">
-      <div className={`flex align-center justify-center mb-6 `}>
-        <p
-          className={` text-6xl transition-transform motion-reduce:transform-none`}
-        >
-          {(query.length > 0 &&
-            query[0].toLocaleUpperCase() + query.slice(1, query.length)) ||
-            abc[current]}
-        </p>
-      </div>
-      <Search returnValue={setQuery} />
-
-      <div className={`flex  justify-between mb-6 text-2xl font-semibold`}>
-        {abc.map((val, idx) => {
-          return alphabet(val, idx);
-        })}{" "}
-      </div>
-      <div
-        className={`overflow-y-auto max-w-7xl min-w-7xl justify-between mb-1`}
-      >
-        {data &&
-          data.map((word: any, idx: number) => {
-            const { definition, partOfSpeech, synonyms, typeOf } = word;
-            return (
-              <div key={idx} className={`flex items-center mb-8`}>
-                <p className="mr-4 text-4xl text-bold">{idx}</p>
-                <div className="flex-row">
-                  <div className="flex  m-0 ">
-                    <p
-                      className={`m-0 text-sm italic opacity-90 mr-3`}
-                    >{`(${partOfSpeech})`}</p>
-
-                    <span className={`flex`}>
-                      {typeOf &&
-                        typeOf.map((text: string, idx: number) => {
-                          return (
-                            <p
-                              key={idx}
-                              className={`m-0 text-sm mr-1 not-italic`}
-                            >
-                              {text}
-                            </p>
-                          );
-                        })}
-                    </span>
-                  </div>
-                  <p className={`mb-3 text-xl `}>{definition}</p>
-                  <div className={`flex`}>
-                    [
-                    {synonyms
-                      ? synonyms[0]
-                          .split(" ")
-                          .map((text: string, idx: number) => {
-                            return (
-                              <p key={idx} className={`m-0 text-sm opacity-90`}>
-                                {`${text} `}
-                              </p>
-                            );
-                          })
-                      : " ... "}
-                    ]
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
+    <div className="flex flex-col">
+      {aZ.map((letter, index) => {
+        const groupedWords = words.filter((word) => word.startsWith(letter));
+        return (
+          <section
+            key={index}
+            id={`letter-${letter}`}
+            className={`w-full justify-between py-24 ${
+              index !== aZ.length - 1 && "border-b"
+            } border-black grid grid-cols-4`}
+          >
+            <div className="relative">
+              <p className="text-5xl ">{letter}</p>
+              <p className="absolute text-rose-600 top-0 left-8 text-md font-black">
+                {groupedWords.length}
+              </p>
+            </div>
+            <div className="w-full col-span-3 gap-6 grid grid-cols-2">
+              {groupedWords.length > 0 ? (
+                groupedWords.map((word) => (
+                  <Link
+                    href={`/${word.toLocaleLowerCase()}`}
+                    className="group w-fit"
+                  >
+                    <p className="cursor-pointer">{word}</p>
+                    <div className="transition-all duration-300 h-2 border-b-2 border-b-red-400 group-hover:w-full w-0" />
+                  </Link>
+                ))
+              ) : (
+                <p className="col-span-2">{`No terms are currently indexed for the letter –> [${letter}]`}</p>
+              )}
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 };
